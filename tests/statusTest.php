d917bb19e798cafacbe92fd78cfd17ee2b287e54
Jordi <jmdesarrollo82@gmail.com>
2020-08-17 15:52:43 +0200

    Add the phpunit xml file

---
+++ tests/statusTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class statusTest extends TestCase
+{
+    public function testIfTestsAreRunningFine(): void
+    {
+        self::assertTrue(true);
+    }
+}
